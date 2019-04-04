7076693066ac43e95f934fc487c5cc1015194a70
ciberciv <ciberciv@gmail.com>
2019-04-04 10:37:15 +0200

    Change post to put request to add the created collection to users table too

--- server.js
+++ server.js
@@ -120,35 +120,7 @@ app.post("/signup", (req, res) => {
   }).catch(err => res.status(400).json("Please, try again"));
 })
 
-app.put("/user/collections", (req, res) => {
-  const {id} = req.body;
-  const token = req.headers.authorization.slice(7);
-  const owner = getUser(token);
-
-  if (!owner) {
-    return res.status(400).json("Something went wrong");
-  }
-
-  return db.select("collection_ids").from("users").where("username", "=", owner)
-    .then(fetchedCollections => {
-      let collections = fetchedCollections[0].collection_ids;
-
-      collections.push(id);
-
-      return collections;
-    })
-    .then(updatedCollections => {
-      console.log(updatedCollections);
-      return db("users").where("username", "=", owner).update({
-        collection_ids: updatedCollections
-      }).returning("collection_ids")
-    })
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(400).json("Something went wrong"))
-
-})
-
-app.post("/collection", (req, res) => {
+app.put("/collection", (req, res) => {
   const {game, name} = req.body;
   const token = req.headers.authorization.slice(7);
   const owner = getUser(token);
@@ -157,21 +129,39 @@ app.post("/collection", (req, res) => {
     return res.status(400).json("Something went wrong user");
   }
 
-  return db.insert({
-    game: game,
-    owner: owner,
-    name: name,
-    card_ids: []
-  })
-    .into("collections")
-    .returning("id")
-    .then(id => {
-      if (!id) {
-        return res.status(400).json("Something went wrong")
-      } else {
-        return res.status(200).json(id)
-      }
-    }).catch(err => res.status(400).json(err))
+  return db.transaction(trx => {
+    trx.insert({
+      game: game,
+      owner: owner,
+      name: name,
+      card_ids: []
+    })
+      .into("collections")
+      .returning("id")
+      .then(id => {
+        if (!id) {
+          return res.status(400).json("Something went wrong")
+        } else {
+          return db.select("collection_ids").from("users").where("username", "=", owner)
+            .then(fetchedCollections => {
+              let collections = fetchedCollections[0].collection_ids;
+
+              console.log(collections);
+
+              collections.push(id[0]);
+
+              return collections;
+            })
+            .then(updatedCollections => {
+              return trx("users").where("username", "=", owner).update({
+                collection_ids: updatedCollections
+              }).returning("collection_ids")
+            })
+            .then(trx.commit).catch(trx.callback)
+        }
+      })
+      .then(data => res.status(200).json("Cool")).catch(err => res.status(400).json("Something went wrong"))
+    })
 })
 
 app.listen(3000);
