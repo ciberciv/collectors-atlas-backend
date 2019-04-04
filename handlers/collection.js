7312f07a415bbd21bdb2c9ade461aaa49581a99e
ciberciv <ciberciv@gmail.com>
2019-04-04 15:23:40 +0200

    Add GET for collection

--- handlers/collection.js
+++ handlers/collection.js
@@ -1,13 +1,35 @@
 const {getUser} = require("../identityManagement");
 const {db} = require("../database");
 
+const get = (req, res) => {
+  const token = req.headers.authorization.slice(7);
+  const owner = getUser(token);
+
+  if (!owner) {
+    return res.status(400).json("Something went wrong");
+  }
+
+  return db.select("collection_ids").from("users").where("username", "=", owner)
+    .then(dbEntry => {
+      userCollections = dbEntry[0].collection_ids;
+
+      let promises = userCollections.map(id => {
+        return db.select("name").from("collections").where("id", "=", id)
+          .then(nameEntry => nameEntry[0].name)
+      })
+
+      return Promise.all(promises).then(collectionNames => collectionNames)
+    })
+    .then(data => res.status(200).json(data)).catch(err => res.status(400).json("Something went wrong"))
+}
+
 const put = (req, res) => {
   const {game, name} = req.body;
   const token = req.headers.authorization.slice(7);
   const owner = getUser(token);
 
   if (!owner) {
-    return res.status(400).json("Something went wrong user");
+    return res.status(400).json("Something went wrong");
   }
 
   return db.transaction(trx => {
@@ -44,5 +66,6 @@ const put = (req, res) => {
 }
 
 module.exports = {
+  get: get,
   put: put
 }
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const handleCollection = require("./handlers/collection");
 app.post("/signin", (req, res) => handleSignIn.post(req, res))
 app.post("/signup", (req, res) => handleSignUp.post(req, res))
 app.put("/collection", (req, res) => handleCollection.put(req, res))
+app.get("/collection", (req, res) => handleCollection.get(req, res))
 
 // Start app
 app.listen(3000);
